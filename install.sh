abecdd503e0463766292bf77551c93301ebe39ea
Bryan Irace <bryan.irace@gmail.com>
2011-12-12 21:27:32 -0500

    Change directory

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-curl -L https://github.com/irace/webzap/tarball/master | tar -xv;
-cd irace-webzap-*;
+curl -L https://github.com/irace/webzap/tarball/master | tar xv;
+mv irace-webzap-* webzap;
+cd webzap;
 easy_install Flask;
 python webzap.py;
